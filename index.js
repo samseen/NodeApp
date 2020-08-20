54530f9c46a3590fb36120a056f92c837028493d
Samson Akanbi <samson.akanbi@outlook.com>
2020-08-20 17:59:33 +0100

    Used the technology styles for the security.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // jquery method
 $(document).ready(function() {
-    // Owl Carousel Library
+    // Technology Owl Carousel
     $('#tech .owl-carousel').owlCarousel({
+        margin:40,
+        dots: false,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
+
+    // Security Owl Carousel
+    $('#security .owl-carousel').owlCarousel({
         margin:40,
         dots: false,
         nav: true,
